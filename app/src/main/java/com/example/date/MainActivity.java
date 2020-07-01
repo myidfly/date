81b47b5ceee3d3cd46ecc75d8edf4117fe15eb19
USER <myidfly@gmail.com>
2020-07-01 18:53:07 +0900

    dasd

--- app/src/main/java/com/example/date/MainActivity.java
+++ app/src/main/java/com/example/date/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.date;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
+    // 현재시간을 msec 으로 구한다.
+    long now = System.currentTimeMillis();
+    // 현재시간을 date 변수에 저장한다.
+    Date date = new Date(now);
+    // 시간을 나타냇 포맷을 정한다 ( yyyy/MM/dd 같은 형태로 변형 가능 )
+    SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    // nowDate 변수에 값을 저장한다.
+    String formatDate = sdfNow.format(date);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        final TextView textView = findViewById(R.id.textView);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                textView.setText(formatDate);
+
+            }
+        });
+    }
+}
+
+/*public class MainActivity extends Activity {
 
     // 현재시간을 msec 으로 구한다.
     long now = System.currentTimeMillis();
@@ -29,7 +62,7 @@ public class MainActivity extends Activity {
         dateNow = (TextView) findViewById(R.id.dateNow);
         dateNow.setText(formatDate);    // TextView 에 현재 시간 문자열 할당
     }
-}
+}*/
 
 
 
