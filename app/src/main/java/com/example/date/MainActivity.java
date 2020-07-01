9cca973eac0ee404b7929e62e0653f45c8bd1239
USER <myidfly@gmail.com>
2020-07-01 19:04:47 +0900

    git it

--- app/src/main/java/com/example/date/MainActivity.java
+++ app/src/main/java/com/example/date/MainActivity.java
@@ -11,14 +11,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends Activity{
-    // 현재시간을 msec 으로 구한다.
+
     long now = System.currentTimeMillis();
-    // 현재시간을 date 변수에 저장한다.
+   .
     Date date = new Date(now);
-    // 시간을 나타냇 포맷을 정한다 ( yyyy/MM/dd 같은 형태로 변형 가능 )
+
     SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-    // nowDate 변수에 값을 저장한다.
+
     String formatDate = sdfNow.format(date);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
