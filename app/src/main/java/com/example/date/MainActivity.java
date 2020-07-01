17a04a7c60e72356eb43fb3c2de0a94723b338bd
USER <myidfly@gmail.com>
2020-07-01 19:07:05 +0900

    git it

--- app/src/main/java/com/example/date/MainActivity.java
+++ app/src/main/java/com/example/date/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class MainActivity extends Activity{
 
     long now = System.currentTimeMillis();
-   .
+
     Date date = new Date(now);
 
     SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
